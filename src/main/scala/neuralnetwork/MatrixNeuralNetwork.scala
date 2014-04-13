368ba47449253d59d1d06a5c1bda8f4f1d3d0f6d
Jianbo Ye <yelpoo@gmail.com>
2014-04-13 11:22:21 -0400

    add MatrixNN; and Unit Test of Gradient Check

---
+++ src/main/scala/neuralnetwork/MatrixNeuralNetwork.scala
@@ -0,0 +1,142 @@
+// Created by: Jianbo Ye, Penn State University jxy198@psu.edu
+// Last Updated: April 2014
+// Copyright under MIT License
+package neuralnetwork
+import breeze.stats.distributions._
+import scala.concurrent.stm._
+
+abstract class MatrixNeuralNetwork (val inputX: Int, val inputY:Int, val outputX:Int, val outputY:Int)
+	extends NeuralNetwork(inputX*inputY, outputX*outputY){
+
+}
+
+abstract class InstanceOfMatrixNeuralNetwork (override val NN: MatrixNeuralNetwork) 
+	extends InstanceOfNeuralNetwork(NN) {
+  type NeuronMatrix = Weight 
+  def applyMatrix(x: NeuronMatrix, mem:SetOfMemorables): NeuronMatrix 
+  def backpropagateMatrix(eta: NeuronMatrix, mem:SetOfMemorables): NeuronMatrix
+  
+  def apply(x: NeuronVector, mem:SetOfMemorables): NeuronVector = {
+    applyMatrix(x.asWeight(NN.inputX, NN.inputY), mem).vec()
+  }
+  def backpropagate(eta: NeuronVector, mem: SetOfMemorables) = {
+    backpropagateMatrix(eta.asWeight(NN.outputX, NN.outputY), mem).vec()
+  }
+}
+
+/*
+abstract class MatrixSelfTransform (val dimensionX: Int, val dimensionY:Int) 
+	extends MatrixNeuralNetwork(dimensionX, dimensionY, dimensionX, dimensionY) 
+abstract class InstanceOfMatrixSelfTransform (override val NN: MatrixSelfTransform) extends InstanceOfMatrixNeuralNetwork (NN)
+*/
+
+class BiLinearSymmetricNN (val inputTensorDimension: Int, val outputTensorDimension: Int)
+	extends MatrixNeuralNetwork (inputTensorDimension, inputTensorDimension, outputTensorDimension, outputTensorDimension) {
+  type InstanceType <: InstanceOfBiLinearSymmetricNN
+  def create(): InstanceOfBiLinearSymmetricNN = new InstanceOfBiLinearSymmetricNN(this)
+}
+
+class InstanceOfBiLinearSymmetricNN (override val NN: BiLinearSymmetricNN) extends InstanceOfMatrixNeuralNetwork(NN) {
+  val inputTensorDimension = NN.inputTensorDimension
+  val outputTensorDimension= NN.outputTensorDimension
+  val W: Weight = new Weight(outputTensorDimension, inputTensorDimension) 
+  val b: NeuronMatrix = new NeuronMatrix (outputTensorDimension, outputTensorDimension)
+  protected val dW = Ref(new Weight(outputTensorDimension, inputTensorDimension))
+  protected val db = Ref(new NeuronMatrix (outputTensorDimension, outputTensorDimension))
+  
+  type StructureType <: BiLinearSymmetricNN
+  override def setWeights(seed:String, w:WeightVector) : Unit = {
+    if (status != seed) {
+      status = seed
+      w(W, b.vec()) // get optimized weights
+      //dw(dW, db) // dW and db are distributed states 
+      atomic { implicit txn =>
+      dW().set(0.0) // reset derivative of weights
+      db().set(0.0)
+      }
+    }
+  }
+  override def getRandomWeights(seed:String) : NeuronVector = {
+    if (status != seed) {
+      status = seed
+      // initialize W: it behaves quite different for Gaussian and Uniform Sampling
+      val amplitude:Double = scala.math.sqrt(6.0/(outputDimension + inputDimension + 1.0))
+      W := new Weight(outputDimension, inputDimension, new Gaussian(0, 1)) 
+      W:*= amplitude// randomly set W 
+      
+      W.vec() concatenate b.vec() 
+    }else {
+      NullVector
+    }
+  }
+  override def getDerativeOfWeights(seed:String, dw:WeightVector, numOfSamples:Int) : Double = {
+    if (status != seed) {
+      status = seed
+      atomic { implicit txn =>
+      dw.get(dW(), db().vec())//(dW.vec concatenate db) // / numOfMirrors
+      }
+    } else {
+    }
+    0.0
+  }
+  override def init(seed:String, mem:SetOfMemorables) = {
+    if (!mem.isDefinedAt(key) || mem(key).status != seed) {
+      mem += (key -> new Memorable)
+      mem(key).status = seed
+      
+      mem(key).numOfMirrors = 1
+      mem(key).mirrorIndex  = 0
+    }
+    else {      
+      mem(key).numOfMirrors = mem(key).numOfMirrors + 1
+      //println(numOfMirrors)
+    }
+    this
+  }
+  override def allocate(seed:String, mem:SetOfMemorables) ={
+    if (mem(key).status == seed) {
+      mem(key).inputBufferM = new Array[NeuronMatrix] (mem(key).numOfMirrors)
+      mem(key).status = ""
+    } else {}
+    this
+  }
+  
+  def applyMatrix(x: NeuronMatrix, mem:SetOfMemorables) = {
+    assert (x.rows == inputTensorDimension && x.cols == inputTensorDimension)
+    mem(key).mirrorIndex = (mem(key).mirrorIndex - 1 + mem(key).numOfMirrors) % mem(key).numOfMirrors
+    mem(key).inputBufferM(mem(key).mirrorIndex) = x
+    ((W Mult x) MultTrans W) + b
+  }
+  def backpropagateMatrix(eta: NeuronMatrix, mem: SetOfMemorables) = {
+    atomic { implicit txn =>
+    //println(key, mem(key).mirrorIndex, eta.data)
+    dW() = dW() + ((eta Mult W Mult mem(key).inputBufferM(mem(key).mirrorIndex)) Mult 2.0) // dgemm and daxpy
+    db() = db() + eta
+    }
+    mem(key).mirrorIndex = (mem(key).mirrorIndex + 1) % mem(key).numOfMirrors
+    (W TransMult eta) Mult W // dgemv
+  }
+}
+
+class RegularizedBiLinearSymNN (inputTensorDimension: Int, outputTensorDimension: Int, val lambda: Double = 0.0)
+	extends BiLinearSymmetricNN(inputTensorDimension, outputTensorDimension) {
+  type InstanceType = InstanceOfRegularizedBiLinearSymNN
+  override def create(): InstanceOfRegularizedBiLinearSymNN = new InstanceOfRegularizedBiLinearSymNN(this)
+}
+
+class InstanceOfRegularizedBiLinearSymNN (override val NN: RegularizedBiLinearSymNN) 
+	extends InstanceOfBiLinearSymmetricNN(NN) {
+  type StructureType = RegularizedBiLinearSymNN
+  override def getDerativeOfWeights(seed:String, dw:WeightVector, numOfSamples:Int) : Double = {
+    if (status != seed) {
+      status = seed
+      atomic { implicit txn =>
+      dW() = dW() + (W Mult (NN.lambda * numOfSamples))
+      dw.get(dW(), db().vec())//(dW.vec + W.vec * (NN.lambda)) concatenate db
+      }
+      W.euclideanSqrNorm * (NN.lambda /2)
+    } else {
+      0.0
+    }    
+  }
+}
---
+++ src/main/scala/tutorials/MatrixNNTest.scala
@@ -0,0 +1,32 @@
+package tutorials
+import breeze.stats.distributions._
+import neuralnetwork._
+
+object MatrixNNTest extends Optimizable with Workspace {
+  def main(args: Array[String]): Unit = {
+    val inputTensorDimension = 10
+    val outputTensorDimension= 10
+    //nn = new BiLinearSymmetricNN(inputTensorDimension,outputTensorDimension).create()
+    val a = new RegularizedBiLinearSymNN(inputTensorDimension,outputTensorDimension, 0.1).create()
+    nn = (a TIMES a).create()
+    val numOfSamples = 1
+    xData = new Array(numOfSamples); yData = new Array(numOfSamples)
+	for (i<- 0 until numOfSamples) {
+	  val xIn = new NeuronVector(inputTensorDimension, new Uniform(-1,1))
+	  val yOut= new NeuronVector(outputTensorDimension, new Uniform(-1,1))
+	  xData(i) =  (xIn CROSS xIn).vec()
+	  yData(i) =  (yOut CROSS yOut).vec()
+	}
+    val w = getRandomWeightVector()		
+    
+    // compute objective and gradient
+    var time = System.currentTimeMillis();
+	val (obj, grad) = getObjAndGrad(w)
+	println(System.currentTimeMillis() - time, obj, grad.data)
+	
+	// gradient checking
+	time = System.currentTimeMillis()
+    val (obj2, grad2) = getApproximateObjAndGrad(w)
+	println(System.currentTimeMillis() - time, obj2, grad2.data)
+  }
+}
