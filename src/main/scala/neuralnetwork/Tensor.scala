7bc5b0d4d6b70e34575529cb4d40f0b745f76f6c
Jianbo Ye <yelpoo@gmail.com>
2014-05-12 03:41:51 -0400

    add many things, but not fully tested

--- src/main/scala/neuralnetwork/Tensor.scala
+++ src/main/scala/neuralnetwork/Tensor.scala
@@ -1,3 +1,4 @@
+// Copyright: MIT License 2014 Jianbo Ye (jxy198@psu.edu)
 package neuralnetwork
 
 // Important change from (a,b) -> (a tensor b, a, b) to (a,b) -> (a tensor b)
@@ -5,6 +6,7 @@ class TensorNeuralNetwork(val firstDimension: Int, val secondDimension: Int)
 	extends NeuralNetwork(firstDimension + secondDimension, firstDimension*secondDimension) {
   type InstanceType = InstanceOfTensorNeuralNetwork
   def create() = new InstanceOfTensorNeuralNetwork(this)
+  def createAdHoc() = new InstanceOfTensorNeuralNetworkAdHoc(this)
 } 
 
 class MTensorNeuralNetwork(val firstDimension: Int, val secondDimension: Int) 
@@ -29,6 +31,7 @@ class InstanceOfTensorNeuralNetwork(override val NN:TensorNeuralNetwork)
   override def allocate(seed:String, mem:SetOfMemorables) ={
     if (mem(key).status == seed) {
       mem(key).inputBuffer= new Array[NeuronVector] (mem(key).numOfMirrors)
+      mem(key).inputBufferM = new Array[NeuronMatrix] (mem(key).numOfMirrors)
       mem(key).status = "" // reset status to make sure *Buffer are allocated only once
     } else {} 
     this
@@ -57,6 +60,93 @@ class InstanceOfTensorNeuralNetwork(override val NN:TensorNeuralNetwork)
     val (firstVec, secondVec) = mem(key).inputBufferM(mem(key).mirrorIndex).spliceRow(NN.firstDimension)
     mem(key).mirrorIndex = (mem(key).mirrorIndex + 1) % mem(key).numOfMirrors
     (ord2GradW Mult secondVec) padRow (ord2GradW TransMult firstVec)
-    etas
   }
 }
+
+class InstanceOfTensorNeuralNetworkAdHoc (override val NN:TensorNeuralNetwork) 
+	extends InstanceOfTensorNeuralNetwork(NN) {
+  override def apply(xs:NeuronMatrix, mem:SetOfMemorables) = {
+    mem(key).mirrorIndex = (mem(key).mirrorIndex + mem(key).numOfMirrors - 1) % mem(key).numOfMirrors
+    mem(key).inputBufferM(mem(key).mirrorIndex) = xs;
+    val (firstVec, secondVec) = xs.spliceRow(NN.firstDimension)
+    firstVec MultTrans secondVec
+  }  
+  override def backpropagate(etas: NeuronMatrix, mem: SetOfMemorables) = {
+    val (firstVec, secondVec) = mem(key).inputBufferM(mem(key).mirrorIndex).spliceRow(NN.firstDimension)
+    mem(key).mirrorIndex = (mem(key).mirrorIndex + 1) % mem(key).numOfMirrors
+    (etas Mult secondVec) padRow (etas TransMult firstVec)
+  }
+}
+
+
+// Important change from (a,b) -> (a tensor b, a, b) to (a,b) -> (a tensor b)
+class SelfTensorNeuralNetwork(val dimension: Int) 
+	extends NeuralNetwork(dimension, dimension*dimension) {
+  type InstanceType = InstanceOfSelfTensorNeuralNetwork
+  def create() = new InstanceOfSelfTensorNeuralNetwork(this)
+  def createAdHoc() = new InstanceOfSelfTensorNeuralNetworkAdHoc(this)
+} 
+
+
+class InstanceOfSelfTensorNeuralNetwork(override val NN:SelfTensorNeuralNetwork) 
+	extends InstanceOfNeuralNetwork(NN) {
+  override def init(seed:String, mem:SetOfMemorables) = {
+    if (!mem.isDefinedAt(key) || mem(key).status != seed) {
+      mem. += (key -> new Memorable)
+      mem(key).status = seed
+      mem(key).numOfMirrors = 1 // find a new instance
+      mem(key).mirrorIndex = 0
+    }
+    else {      
+      mem(key).numOfMirrors = mem(key).numOfMirrors + 1
+    }
+    this
+  }
+  
+  override def allocate(seed:String, mem:SetOfMemorables) ={
+    if (mem(key).status == seed) {
+      mem(key).inputBuffer= new Array[NeuronVector] (mem(key).numOfMirrors)
+      mem(key).inputBufferM = new Array[NeuronMatrix] (mem(key).numOfMirrors)
+      mem(key).status = "" // reset status to make sure *Buffer are allocated only once
+    } else {} 
+    this
+  }
+  def apply(x:NeuronVector, mem:SetOfMemorables) = {
+    mem(key).mirrorIndex = (mem(key).mirrorIndex + mem(key).numOfMirrors - 1) % mem(key).numOfMirrors
+    mem(key).inputBuffer(mem(key).mirrorIndex) = x;
+    (x CROSS x).vec() // concatenate firstVec concatenate secondVec
+  }
+  def apply(xs:NeuronMatrix, mem:SetOfMemorables) = {
+    mem(key).mirrorIndex = (mem(key).mirrorIndex + mem(key).numOfMirrors - 1) % mem(key).numOfMirrors
+    mem(key).inputBufferM(mem(key).mirrorIndex) = xs;
+    (xs CROSS xs).mat()
+  }
+  def backpropagate(eta: NeuronVector, mem: SetOfMemorables) = {
+    val ord2GradW = eta.asNeuronMatrix(NN.dimension, NN.dimension) //change ord2Grad -> eta (only)
+    val x = mem(key).inputBuffer(mem(key).mirrorIndex)
+    mem(key).mirrorIndex = (mem(key).mirrorIndex + 1) % mem(key).numOfMirrors
+    (ord2GradW + ord2GradW.transpose) Mult x
+  }
+  def backpropagate(etas: NeuronMatrix, mem: SetOfMemorables) = {
+    // INCOMPLETE: To be implemented
+    val ord2GradW = etas.asNeuronTensor(NN.dimension, NN.dimension)
+    val xs = mem(key).inputBufferM(mem(key).mirrorIndex)
+    mem(key).mirrorIndex = (mem(key).mirrorIndex + 1) % mem(key).numOfMirrors
+    (ord2GradW Mult xs) + (ord2GradW TransMult xs)
+  }
+}
+
+class InstanceOfSelfTensorNeuralNetworkAdHoc (override val NN:SelfTensorNeuralNetwork) 
+	extends InstanceOfSelfTensorNeuralNetwork(NN) {
+  override def apply(xs:NeuronMatrix, mem:SetOfMemorables) = {
+    mem(key).mirrorIndex = (mem(key).mirrorIndex + mem(key).numOfMirrors - 1) % mem(key).numOfMirrors
+    mem(key).inputBufferM(mem(key).mirrorIndex) = xs;
+    (xs MultTrans xs) / xs.cols
+  }  
+  override def backpropagate(etas: NeuronMatrix, mem: SetOfMemorables) = {
+    val xs = mem(key).inputBufferM(mem(key).mirrorIndex)
+    mem(key).mirrorIndex = (mem(key).mirrorIndex + 1) % mem(key).numOfMirrors
+    ((etas + etas.transpose) Mult xs) / xs.cols
+  }
+}
+	
