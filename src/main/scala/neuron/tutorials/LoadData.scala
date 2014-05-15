563cadcb680997145f5cc56ec596fff1664eaf33
Jianbo Ye <yelpoo@gmail.com>
2014-05-15 19:52:18 -0400

    load data in Array[DenseVector[Double]]

--- src/main/scala/neuron/tutorials/LoadData.scala
+++ src/main/scala/neuron/tutorials/LoadData.scala
@@ -3,6 +3,7 @@ import neuron.math._
 import breeze.linalg._
 
 object LoadData {
+  
   def rawImages64(): Array[NeuronVector] = {
       val numOfPixels = 8*8	  
 	  val source = scala.io.Source.fromFile("data/UFLDL/sparseae/patches64x10000.txt")
@@ -17,6 +18,20 @@ object LoadData {
       xData
   }
   
+  def rawImages64V(): Array[DenseVector[Double]] = {
+      val numOfPixels = 8*8	  
+	  val source = scala.io.Source.fromFile("data/UFLDL/sparseae/patches64x10000.txt")
+	  val dataSource = source.getLines.toArray
+	  val numOfSamples = dataSource.length
+	  val xData = new Array[DenseVector[Double]](numOfSamples)
+	  for (i<- 0 until numOfSamples) {
+	    xData(i) = 
+	        new DenseVector(dataSource(i).split("\\s+").map(_.toDouble), 0, 1, numOfPixels)
+	  }
+      source.close()
+      xData    
+  }
+
   def rawImages64M(): NeuronMatrix = {
       val numOfPixels = 8*8	  
 	  val source = scala.io.Source.fromFile("data/UFLDL/sparseae/patches64x10000.txt")
@@ -45,6 +60,25 @@ object LoadData {
     xData
   }
   
+  def mnistTrainV(): Array[DenseVector[Double]] = {
+        // Load MNIST data
+    import java.io._
+    val source = new DataInputStream(new FileInputStream("data/UFLDL/sparseae/mnist/train-images.idx3-ubyte"))
+    println("magic: " + source.readInt())
+    val numOfSamples = source.readInt(); println("numOfImages: " + numOfSamples)
+    val numOfRows = source.readInt(); println("numOfRows: " + numOfRows)
+    val numOfCols = source.readInt(); println("numOfCols: " + numOfCols)
+    val numOfPixels = numOfRows * numOfCols
+    
+    val xData = new Array[DenseVector[Double]](numOfSamples)
+    for (i<- 0 until numOfSamples) {
+      val buf = new Array[Byte](numOfPixels)
+      source.read(buf)
+      xData(i) = new DenseVector(buf.map(b => (0xff & b).toDouble/255.00), 0, 1, numOfPixels)
+    }
+    source.close()
+    xData
+  }  
   def mnistTrainM(): NeuronMatrix = {
     import java.io._
     val source = new DataInputStream(new FileInputStream("data/UFLDL/sparseae/mnist/train-images.idx3-ubyte"))
